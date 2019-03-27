29eefb2fff6a2baabf4239880e6ae53a5c2f6db7
lordwarlock <daniel_zzh1991@hotmail.com>
2019-03-27 00:16:02 -0400

    combine unet and big gan setting

--- UNET.py
+++ UNET.py
@@ -1,6 +1,126 @@
-from Pix2PixUNet import UnetGenerator
+from Pix2PixUNet import UnetSkipConnectionBlock
+import torch.nn as nn
 from BigGAN import Discriminator
 from BigGAN import G_D
-class Generator(UnetGenerator):
-	def __init__(self, **kwargs):
-		super(Generator, self).__init__(1, 3, 7)
+class Generator(nn.Module):
+  def __init__(self, G_ch=64, dim_z=128, bottom_width=4, resolution=128,
+               G_kernel_size=3, G_attn='64', n_classes=1000,
+               num_G_SVs=1, num_G_SV_itrs=1,
+               G_shared=True, shared_dim=0, hier=False,
+               cross_replica=False, mybn=False,
+               G_activation=nn.ReLU(inplace=False),
+               G_lr=5e-5, G_B1=0.0, G_B2=0.999, adam_eps=1e-8,
+               BN_eps=1e-5, SN_eps=1e-12, G_mixed_precision=False, G_fp16=False,
+               G_init='ortho', skip_init=False, no_optim=False,
+               G_param='SN', norm_style='bn',
+               **kwargs):
+    super(Generator, self).__init__()
+    # Channel width mulitplier
+    self.ch = G_ch
+    # Dimensionality of the latent space
+    self.dim_z = dim_z
+    # The initial spatial dimensions
+    self.bottom_width = bottom_width
+    # Resolution of the output
+    self.resolution = resolution
+    # Kernel size?
+    self.kernel_size = G_kernel_size
+    # Attention?
+    self.attention = G_attn
+    # number of classes, for use in categorical conditional generation
+    self.n_classes = n_classes
+    # Use shared embeddings?
+    self.G_shared = G_shared
+    # Dimensionality of the shared embedding? Unused if not using G_shared
+    self.shared_dim = shared_dim if shared_dim > 0 else dim_z
+    # Hierarchical latent space?
+    self.hier = hier
+    # Cross replica batchnorm?
+    self.cross_replica = cross_replica
+    # Use my batchnorm?
+    self.mybn = mybn
+    # nonlinearity for residual blocks
+    self.activation = G_activation
+    # Initialization style
+    self.init = G_init
+    # Parameterization style
+    self.G_param = G_param
+    # Normalization style
+    self.norm_style = norm_style
+    # Epsilon for BatchNorm?
+    self.BN_eps = BN_eps
+    # Epsilon for Spectral Norm?
+    self.SN_eps = SN_eps
+    # fp16?
+    self.fp16 = G_fp16
+    # Architecture dict
+
+    self.dim_z = 128 * 128 * 3
+
+    # Prepare model
+    input_nc = 1
+    output_nc = 3
+    num_downs = 7
+    ngf=64
+    norm_layer=nn.BatchNorm2d
+    use_dropout=False
+    # construct unet structure
+    unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)  # add the innermost layer
+    for i in range(num_downs - 5):          # add intermediate layers with ngf * 8 filters
+        unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer, use_dropout=use_dropout)
+    # gradually reduce the number of filters from ngf * 8 to ngf
+    unet_block = UnetSkipConnectionBlock(ngf * 4, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
+    unet_block = UnetSkipConnectionBlock(ngf * 2, ngf * 4, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
+    unet_block = UnetSkipConnectionBlock(ngf, ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
+    self.model = UnetSkipConnectionBlock(output_nc, ngf, input_nc=input_nc, submodule=unet_block, outermost=True, norm_layer=norm_layer)  # add the outermost layer
+
+    # Initialize weights. Optionally skip init for testing.
+    if not skip_init:
+      self.init_weights()
+
+    # Set up optimizer
+    # If this is an EMA copy, no need for an optim, so just return now
+    if no_optim:
+      return
+    self.lr, self.B1, self.B2, self.adam_eps = G_lr, G_B1, G_B2, adam_eps
+    if G_mixed_precision:
+      print('Using fp16 adam in G...')
+      import utils
+      self.optim = utils.Adam16(params=self.parameters(), lr=self.lr,
+                           betas=(self.B1, self.B2), weight_decay=0,
+                           eps=self.adam_eps)
+    else:
+      self.optim = optim.Adam(params=self.parameters(), lr=self.lr,
+                           betas=(self.B1, self.B2), weight_decay=0,
+                           eps=self.adam_eps)
+
+    # LR scheduling, left here for forward compatibility
+    # self.lr_sched = {'itr' : 0}# if self.progressive else {}
+    # self.j = 0
+
+  # Initialize
+  def init_weights(self):
+    self.param_count = 0
+    for module in self.modules():
+      if (isinstance(module, nn.Conv2d) 
+          or isinstance(module, nn.Linear) 
+          or isinstance(module, nn.Embedding)):
+        if self.init == 'ortho':
+          init.orthogonal_(module.weight)
+        elif self.init == 'N02':
+          init.normal_(module.weight, 0, 0.02)
+        elif self.init in ['glorot', 'xavier']:
+          init.xavier_uniform_(module.weight)
+        else:
+          print('Init style not recognized...')
+        self.param_count += sum([p.data.nelement() for p in module.parameters()])
+    print('Param count for G''s initialized parameters: %d' % self.param_count)
+
+  # Note on this forward function: we pass in a y vector which has
+  # already been passed through G.shared to enable easy class-wise
+  # interpolation later. If we passed in the one-hot and then ran it through
+  # G.shared in this forward function, it would be harder to handle.
+  def forward(self, z, y):
+    """Standard forward"""
+    z = z.view(-1, 3, 128, 128)
+    return self.model(z)
